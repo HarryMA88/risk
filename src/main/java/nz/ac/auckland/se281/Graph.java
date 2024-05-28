fb860d566cb3c906437f82c88f3a2a150e3d37d9
Harry <hma481@aucklanduni.ac.nz>
2024-05-28 18:53:31 +1200

    created Country class and a class for graph for the map

---
+++ src/main/java/nz/ac/auckland/se281/Graph.java
@@ -0,0 +1,38 @@
+package nz.ac.auckland.se281;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Graph {
+  private Map<Country, List<Country>> adjNodes;
+
+  public Graph() {
+    this.adjNodes = new HashMap<>();
+  }
+
+  public void addNode(Country country) {
+    adjNodes.putIfAbsent(country, new ArrayList<Country>());
+  }
+
+  public void addEdge(Country countryA, Country countryB) {
+    addNode(countryA);
+    addNode(countryB);
+
+    adjNodes.get(countryA).add(countryB);
+    adjNodes.get(countryB).add(countryA);
+  }
+
+  public void removeNode(Country country) {
+    adjNodes.remove(country);
+    for (Country node : adjNodes.keySet()) {
+      adjNodes.get(node).remove(country);
+    }
+  }
+
+  public void removeEdge(Country countryA, Country countryB) {
+    adjNodes.getOrDefault(countryA, new ArrayList<>()).remove(countryB);
+    adjNodes.getOrDefault(countryB, new ArrayList<>()).remove(countryA);
+  }
+}
--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -4,15 +4,22 @@ import java.util.List;
 
 /** This class is the main entry point. */
 public class MapEngine {
+  Graph map;
 
   public MapEngine() {
     // add other code here if you want
+    map = new Graph();
     loadMap(); // keep this mehtod invocation
   }
 
   /** invoked one time only when constracting the MapEngine class. */
   private void loadMap() {
     List<String> countries = Utils.readCountries();
+    // parse each string in the list
+    for (String country : countries) {
+      String[] countryInfo  = country.split(",");
+      
+    }
     List<String> adjacencies = Utils.readAdjacencies();
     // add code here to create your data structures
   }
